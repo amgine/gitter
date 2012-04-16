489f9a40d85d9f6c4d2c93c8fddefa06e68dbdf6
max <amgine.gitter@gmail.com>
2012-04-17 03:32:34 +0400

    added missing CommonAssemblyInfo.cs

---
+++ common/CommonAssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Resources;
+using System.Reflection;
+
+[assembly: AssemblyCompany("amgine")]
+[assembly: AssemblyProduct("gitter")]
+[assembly: AssemblyCopyright("Copyright © amgine 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: CLSCompliant(true)]
+
+[assembly: NeutralResourcesLanguage("en-us")]
+
+[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.1")] 
