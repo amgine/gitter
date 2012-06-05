69d964172216ee2fa8d56b2c70d8bb9700714795
max <amgine.gitter@gmail.com>
2012-06-05 17:19:23 +0400

    version bump
    stability improvements

--- common/CommonAssemblyInfo.cs
+++ common/CommonAssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Reflection;
 
 [assembly: NeutralResourcesLanguage("en-us")]
 
-[assembly: AssemblyFileVersion("1.0.4.0")]
-[assembly: AssemblyVersion("1.0.4.0")] 
+[assembly: AssemblyFileVersion("1.0.5.0")]
+[assembly: AssemblyVersion("1.0.5.0")] 
