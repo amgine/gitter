2348ae265128cc924ef57265f4f5277f02334488
max <amgine.gitter@gmail.com>
2013-11-28 12:13:17 +0400

    version bump

--- common/CommonAssemblyInfo.cs
+++ common/CommonAssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Reflection;
 
 [assembly: NeutralResourcesLanguage("en-us")]
 
-[assembly: AssemblyFileVersion("1.0.14.0")]
-[assembly: AssemblyVersion("1.0.14.0")] 
+[assembly: AssemblyFileVersion("1.0.15.0")]
+[assembly: AssemblyVersion("1.0.15.0")] 
