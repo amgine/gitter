c840345185419edbaaa373ae32782261e6d7eb1e
max <amgine.gitter@gmail.com>
2020-10-02 12:57:35 +0300

    version bump

--- common/CommonAssemblyInfo.cs
+++ common/CommonAssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Reflection;
 
 [assembly: NeutralResourcesLanguage("en-us")]
 
-[assembly: AssemblyFileVersion("1.0.21.0")]
-[assembly: AssemblyVersion("1.0.21.0")] 
+[assembly: AssemblyFileVersion("1.0.22.0")]
+[assembly: AssemblyVersion("1.0.22.0")] 
