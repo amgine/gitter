ec1322228a675e27a4fba9a3338da2d001bb4dcc
Yaroslav <qwerty_qwerty12345678910@mail.ru>
2026-05-12 21:37:34 +0300

    v1.3.1 gitlab: Label POCO + project-labels endpoint + tests

--- gitter.gitlab.prj/Api/ApiEndpoint.Labels.cs
+++ gitter.gitlab.prj/Api/ApiEndpoint.Labels.cs
@@ -1,41 +1,43 @@
-﻿#region Copyright Notice
+#region Copyright Notice
 /*
  * gitter - VCS repository management tool
- * Copyright (C) 2020  Popovskiy Maxim Vladimirovitch <amgine.gitter@gmail.com>
- * 
+ * Copyright (C) 2013  Popovskiy Maxim Vladimirovitch <amgine.gitter@gmail.com>
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #endregion
 
-using System;
-using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
+namespace gitter.GitLab.Api;
 
-[assembly: AssemblyCompany("amgine")]
-[assembly: AssemblyProduct("gitter")]
-[assembly: AssemblyCopyright("Copyright © amgine 2025")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
-[assembly: NeutralResourcesLanguage("en-us")]
-
-[assembly: AssemblyFileVersion("1.3.1.0")]
-[assembly: AssemblyVersion("1.3.1.0")]
-
-#if NET6_0_OR_GREATER
-[module: System.Runtime.Versioning.SupportedOSPlatform("windows")]
-#endif
-
-[assembly: InternalsVisibleTo("gitter.tests")]
+partial class ApiEndpoint
+{
+	public Task<IReadOnlyList<Label>> GetProjectLabelsAsync(
+		NameOrNumericId projectId,
+		bool withCounts = false,
+		CancellationToken cancellationToken = default)
+	{
+		var query = new StringBuilder();
+		AppendProjectUrl(query, projectId, @"labels");
+		if(withCounts)
+		{
+			query.Append("?with_counts=true");
+		}
+		return ReadPagedResultAsync<Label>(query.ToString(), cancellationToken);
+	}
+}
